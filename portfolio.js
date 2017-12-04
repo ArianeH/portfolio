3171607b32aff41215621fdb8f181e6c1fbe783e
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-12-04 19:41:21 +0100

    Added further scss files for each page and component of the application and added basic content to portfolio page and navbar component

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $(function(){
     $("#nav-placeholder").load("shared/navbar.html");
+    $("#portfolio-placeholder").load("views/portfolio.html");
     $("#footer-placeholder").load("shared/footer.html");
   });
 
