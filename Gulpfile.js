ce8d7387b4d2aef1c5f02aa399069cfd09f31b60
Ariane Hartmann <ariane.ermelind.hartmann@gmail.com>
2017-12-06 20:42:09 +0100

    Adjusted display of cards in the portfolio section

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,8 +3,9 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(watch('sass/**/*.scss',['styles']))
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+  gulp.src('sass/**/*.scss')
+  // .pipe(watch('sass/**/*.scss',['styles']))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./css/'));
+  gulp.watch('sass/**/*.scss',['styles']);
 });
--- portfolio.js
+++ portfolio.js
@@ -3,36 +3,25 @@ $(document).ready(function() {
   $.getJSON("../project-info.json", function(json){
     projectInfo = json;
 
-  //   $("#portfolio-section").append('<div class="container"></div>');
-
-  //   $(".container").append('<div class="card-wrapper"></div>');
-  $.each(projectInfo, function(index, element) {
-    console.log(element.title, element.description);
-
-
-  // $(function(){
-  //   $("#portfolio-placeholder").load("views/portfolio.html");
-  // });
-
-
   // Grab the template script
   var theTemplateScript = $("#card-template").html();
 
   // Compile the template
   var theTemplate = Handlebars.compile(theTemplateScript);
 
+
   // Define our data object
-  var context={
-    "title": element.title,
-    "description": element.description
-  };
+  // var context={
+  //   "title": element.title,
+  //   "description": element.description
+  // };
 
   // Pass our data to the template
-  var theCompiledHtml = theTemplate(context);
+  var theCompiledHtml = theTemplate(projectInfo);
 
   // Add the compiled html to the page
   $('.card-placeholder').html(theCompiledHtml);
-    });
+    // });
 });
 
 
